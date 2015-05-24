e53413af344955bb0dfaf2100d05d10f79ac584f
Genki Sugawara <sugawara@cookpad.com>
2015-05-24 14:18:19 +0900

    first commit

---
+++ lib/fluent/plugin/elb/access/log.rb
@@ -0,0 +1,13 @@
+require "fluent/plugin/elb/access/log/version"
+
+module Fluent
+  module Plugin
+    module Elb
+      module Access
+        module Log
+          # Your code goes here...
+        end
+      end
+    end
+  end
+end
